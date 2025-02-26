c9cba4af2755ce30ac85f6a0a97483cca1ccf48e
ftp <proskuks_ftp@turbot.beget.ru>
2025-02-26 16:06:58 +0300

    Re-add bitrix/components/aspro

---
+++ bitrix/components/aspro/oneclickbuy.next/lang/en/script.php
@@ -0,0 +1,27 @@
+<?
+$MESS['NO_USER_NAME'] = 'Не указано имя.';
+$MESS['NO_PHONE'] = 'Не указан или неправильно указан телефон.';
+$MESS['TOO_MANY_USERS_WITH_PHONE'] = 'Ошибка регистрации нового пользователя: пользователь с номером телефона #PHONE# уже существует.';
+$MESS['PHONE_REGISTER_CODE_VERIFY_ERROR'] = 'Неверный код из СМС.';
+$MESS['ONE_CLICK_SMS_SENDED'] = 'На ваш номер было выслано СМС с кодом подтверждения.';
+$MESS['CURRENCY_NOT_FOUND'] = 'Валюта не найдена';
+$MESS['AUTH_FAIL'] = 'Ошибка авторизации';
+$MESS['TOO_MANY_USERS'] = 'Найдено более 1 пользователя с указанным email.';
+$MESS['USER_REGISTER_FAIL'] = 'Ошибка регистрации пользователя.';
+$MESS['ORDER_CREATE_FAIL'] = 'Ошибка создания заказа.';
+$MESS['PRODUCT_PRICE_NOT_FOUND'] = 'Цена на товар не найдена.';
+$MESS['ITEM_ADD_FAIL'] = 'Ошибка добавления товара в заказ.';
+$MESS['ITEM_UPDATE_FAIL'] = 'Ошибка присвоения номера заказа товару в корзине.';
+$MESS['ORDER_CREATE_SUCCESS'] = 'Заказ успешно оформлен.';
+$MESS['PHONE'] = 'Телефон для связи: ';
+$MESS['ITEM_NAME'] = 'Название: ';
+$MESS['ITEM_PRICE'] = ', цена за единицу: ';
+$MESS['ITEM_QTY'] = ', количество: ';
+$MESS['ITEM_TOTAL'] = ', стоимость: ';
+$MESS['NO_PROPER_DATA'] = 'Недостаточно данных';
+$MESS['FAST_ORDER_COMMENT'] = 'Быстрый заказ';
+$MESS['BAD_EMAIL_FORMAT'] = 'Неверный формат email адреса';
+$MESS['CAPTCHA_ERROR_CODE'] = 'Неверный код проверки';
+$MESS['CANT_INCLUDE_MODULE'] = 'Не подключены необходимые модули';
+$MESS['CAPTCHA_LABEL'] = 'Введите текст с картинки';
+?>
